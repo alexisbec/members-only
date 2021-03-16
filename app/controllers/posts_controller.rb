039c79b5b395055ed9f4712ba76ab1c0571ace61
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-16 14:31:55 -0600

    edit style for form fields, add validations for sign up form, and edit sign up form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   # GET /posts/1 or /posts/1.json
