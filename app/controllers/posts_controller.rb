dfd87581ed51b539dc28bdb149c0cd1db3d49409
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-16 14:52:10 -0600

    edit the signup and new post form, add validation to new post, and add :title param for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:title, :content)
     end
 end
