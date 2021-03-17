05a594d5b89443f09156014fb288badce593c051
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-17 12:36:22 -0600

    fix css and ruby linter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
   def signup_link_path
     if user_signed_in?
-      link_to(current_user.author, edit_user_registration_path, class: "white-btn")
+      link_to(current_user.author, edit_user_registration_path, class: 'white-btn')
     else
-      link_to('Sign Up', new_user_registration_path, class: "white-btn")
+      link_to('Sign Up', new_user_registration_path, class: 'white-btn')
     end
   end
 
   def session_link_path
     if user_signed_in?
-      link_to("Logout", destroy_user_session_path, method: :delete, class: "white-btn")
+      link_to('Logout', destroy_user_session_path, method: :delete, class: 'white-btn')
     else
-      link_to('Log In', new_user_session_path, class: "white-btn")
+      link_to('Log In', new_user_session_path, class: 'white-btn')
     end
   end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,25 +1,17 @@
 module PostsHelper
   def show_author(post)
-    if user_signed_in?
-      "by #{post.user.author}"
-    end
+    "by #{post.user.author}" if user_signed_in?
   end
 
   def edit_post(post)
-    if user_signed_in?
-      link_to('Edit', edit_post_path(post))
-    end
+    link_to('Edit', edit_post_path(post)) if user_signed_in?
   end
 
   def destroy_post(post)
-    if user_signed_in?
-      link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' })
-    end
+    link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }) if user_signed_in?
   end
 
   def new_post
-    if user_signed_in?
-      link_to('New Post', new_post_path, class: "new")
-    end
+    link_to('New Post', new_post_path, class: 'new') if user_signed_in?
   end
 end
