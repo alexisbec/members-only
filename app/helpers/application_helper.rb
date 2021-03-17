3203fd4a339a924a7114e3ffbbc150728c9ba3c3
Alexis Varela <alexisbec13@hotmail.com>
2021-03-17 11:44:09 -0600

    remove logic from views and add it inside helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,44 +1,17 @@
 module ApplicationHelper
-  def signup_link_name(user)
-    if user_signed_in?
-      user.author
-    else
-      "Sign Up"
-    end
-  end
-
   def signup_link_path
     if user_signed_in?
-      edit_user_registration_path
+      link_to(current_user.author, edit_user_registration_path, class: "white-btn")
     else
-      new_user_registration_path
-    end
-  end
-
-  def session_link_name
-    if user_signed_in?
-      "Logout"
-    else
-      "Log In"
+      link_to('Sign Up', new_user_registration_path, class: "white-btn")
     end
   end
 
   def session_link_path
     if user_signed_in?
-      destroy_user_session_path
+      link_to("Logout", destroy_user_session_path, method: :delete, class: "white-btn")
     else
-      new_user_session_path
+      link_to('Log In', new_user_session_path, class: "white-btn")
     end
   end
 end
-
-
-
-
-# <% if user_signed_in? %>
-#   <span><%= link_to current_user.author, edit_user_registration_path, class: "white-btn" %></span>
-#   <span><%= link_to "Logout", destroy_user_session_path, method: :delete, class: "white-btn" %></span>
-# <% else %>
-#   <span><%= link_to 'Sign Up', new_user_registration_path, class: "white-btn" %></span>
-#   <span><%= link_to 'Log In', new_user_session_path, class: "white-btn" %></span>
-# <% end %>
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,25 @@
 module PostsHelper
+  def show_author(post)
+    if user_signed_in?
+      post.user.author
+    end
+  end
+
+  def edit_post(post)
+    if user_signed_in?
+      link_to('Edit', edit_post_path(post))
+    end
+  end
+
+  def destroy_post(post)
+    if user_signed_in?
+      link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' })
+    end
+  end
+
+  def new_post
+    if user_signed_in?
+      link_to('New Post', new_post_path, class: "new")
+    end
+  end
 end
