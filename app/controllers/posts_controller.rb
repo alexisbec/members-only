bc7ed00a8dfef55640dd41b8a07693c3dda16b6e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-03-17 18:41:56 -0600

    removed all post actions required by the Code Reviewer, and removed all code that is related to the deleted actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[new create]
 
   # GET /posts or /posts.json
@@ -24,25 +23,6 @@ class PostsController < ApplicationController
       else
         format.html { render :new, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /posts/1 or /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /posts/1 or /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
     end
   end
 
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,14 +3,6 @@ module PostsHelper
     "by #{post.user.author}" if user_signed_in?
   end
 
-  def edit_post(post)
-    link_to('Edit', edit_post_path(post)) if user_signed_in?
-  end
-
-  def destroy_post(post)
-    link_to('Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }) if user_signed_in?
-  end
-
   def new_post
     link_to('New Post', new_post_path, class: 'new') if user_signed_in?
   end
